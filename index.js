3299225467f89aae9c4c7bc79e40f920d50ec18b
naningntch <64010232@kmitl.ac.th>
2024-02-20 13:29:49 +0700

    111

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
 const plusHandler = (req, res) => {
-    const result = parseInt(req.params.num1) + parseInt(req.params.num2);
-    res.json(result);
+  const result = parseInt(req.params.num1) + parseInt(req.params.num2);
+  res.json(result);
 };
 
-app.get("/", (req, res) => res.send("Hello World!"))
-app.get("/getcode", (req, res) => res.send("Oot the most handsome person in the world!!!"))
-app.get("/plus/:num1/:num2",plusHandler)
-const server = app.listen(3001, () => console.log("Example app listening on port 3001!"))
+app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/getcode", (req, res) => res.send("111"));
+app.get("/plus/:num1/:num2", plusHandler);
+const server = app.listen(3001, () =>
+  console.log("Example app listening on port 3001!")
+);
 
 module.exports = {
-    plusHandler,
-    server
-}
+  plusHandler,
+  server,
+};
