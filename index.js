1fc8766a68d823d3b1b16534508c08cf2be8bdfe
naningntch <64010232@kmitl.ac.th>
2024-02-20 13:56:56 +0700

    999

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const plusHandler = (req, res) => {
 };
 
 app.get("/", (req, res) => res.send("Hello World!"));
-app.get("/getcode", (req, res) => res.send("today"));
+app.get("/getcode", (req, res) => res.send("999"));
 app.get("/plus/:num1/:num2", plusHandler);
 const server = app.listen(3001, () =>
   console.log("Example app listening on port 3001!")
